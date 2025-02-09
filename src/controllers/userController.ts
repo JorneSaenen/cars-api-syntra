10253ac1302793882788809afc7637027f31056c
jorne Saenen <jorne@jsjj.be>
2025-02-09 14:37:54 +0100

    add and remove favortites

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -40,7 +40,84 @@ export const getUser = async (req: Request, res: Response) => {
       return vehicle;
     });
 
-    res.status(200).json({ status: "success", data: userFavoritesAndLicenses });
+    res.status(200).json({
+      status: "success",
+      data: { ...user.toObject(), favorites: userFavoritesAndLicenses },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+};
+
+export const addToFavorites = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { vehicleId } = req.body;
+
+    if (id !== req.user?._id) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    if (!vehicleId) {
+      res.status(400).json({ message: "vehicleId in the body is required!" });
+      return;
+    }
+
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $addToSet: { favorites: vehicleId } },
+      { new: true }
+    );
+
+    res.status(200).json({ status: "success", data: updatedUser });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+};
+
+export const removeFromFavorites = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { vehicleId } = req.body;
+
+    if (id !== req.user?._id) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    if (!vehicleId) {
+      res.status(400).json({ message: "vehicleId in the body is required!" });
+      return;
+    }
+
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $pull: { favorites: vehicleId } },
+      { new: true }
+    );
+
+    res.status(200).json({ status: "success", data: updatedUser });
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
