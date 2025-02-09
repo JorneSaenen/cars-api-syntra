fce1af6bd131c29a5a69d78ae04e09d057a207c2
jorne Saenen <jorne@jsjj.be>
2025-02-09 13:36:56 +0100

    remove unneeded env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,9 +3,8 @@ import "dotenv/config";
 export const PORT = process.env.PORT || 3000;
 export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const ARCJET_KEY = process.env.ARCJET_KEY;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
-if (!MONGO_URI || !JWT_SECRET || !ARCJET_KEY) {
+if (!MONGO_URI || !JWT_SECRET) {
   throw new Error("Missing environment variables");
 }
