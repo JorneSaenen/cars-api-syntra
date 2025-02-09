c54e22ea5699e0ce876c21c20b3dd25769077b12
jorne Saenen <jorne@jsjj.be>
2025-02-09 14:38:06 +0100

    add routes for favorites

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { getUser, getUsers } from "../controllers/userController";
+import {
+  addToFavorites,
+  getUser,
+  getUsers,
+  removeFromFavorites,
+} from "../controllers/userController";
 import authMiddleware from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.get("/", getUsers).get("/:id", authMiddleware, getUser);
+router
+  .get("/", getUsers)
+  .get("/:id", authMiddleware, getUser)
+  .post("/add-to-favorites/:id", authMiddleware, addToFavorites)
+  .delete("/remove-from-favorites/:id", authMiddleware, removeFromFavorites);
 
 export default router;
